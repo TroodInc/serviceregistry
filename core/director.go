6aae51fa3d64b8f4fac4fbe4f3492cc4a7940f3f
vkarpov <vkarpov@identityblitz.com>
2017-06-27 14:45:49 +0300

    Added Director

--- core/director.go
+++ core/director.go
@@ -14,6 +14,7 @@ const (
 	ErrDirWrongPort   = "director_wrong_port"
 	ErrDirWrongSrvName = "director_wrong_srv_name"
 	ErrDirWrongSrvType = "director_wrong_srv_type"
+	ErrDirWrongTxtString = "director_wrong_txt_string"
 )
 
 type DirectorError struct {
@@ -80,6 +81,92 @@ func (d *Director) attachSrvToType(srvType, srvName string) (*dns.PTR, error) {
 	return ptr, nil
 }
 
+func (d *Director) assignSrvToServer(srvName string, server string, port uint16, ttl uint32, priority uint16, weight uint16) (*dns.SRV, error) {
+	if e := validateSrvName(srvName); e != nil {
+		return nil, e
+	}
+
+	srv := new(dns.SRV)
+	srv.Hdr = dns.RR_Header{srvName, dns.TypeSRV, dns.ClassINET, ttl, 0}
+	srv.Target = server
+	srv.Port = port
+	srv.Priority = priority
+	srv.Weight = weight
+	return srv, nil
+}
+
+func isNotAllowedTxtKeyCharacter(c rune) bool {
+	return c < 0x20 || c > 0x7e || c == 0x3d
+}
+
+// *recomended full len is 1300
+// *255 is max key-value string length
+// *key size is beetwen 1 and 9
+// *key character is within range 0x20-0x7E, except 0x3D
+// *key is not case sensative
+// *value may be absent
+// *if there is more than one key of the same name, all keys after first one are discarded
+// *txtvers=x
+// protovers is protocol version key
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func (d *Director) addServRules(srvName string, params map[string]string) (*dns.TXT, error) {
+	if e := validateSrvName(srvName); e != nil {
+		return nil, e
+	}
+
+	var str []string = nil
+	if len(params) != 0 {
+		str = make([]string, 1, len(params))
+		var fullLen = 0
+		var strLen int
+		for k, v := range params {
+			if len(k) < 1 || len(k) > 9 {
+				return nil, NewDirectorError(ErrDirWrongTxtString, "TXT key must have length between 1 and 9")
+			}
+			if pos := strings.IndexFunc(srvName, isNotAllowedCharacter); pos != -1 {
+				return nil, NewDirectorError(ErrDirWrongTxtString, "TXT key contains a not allowed character")
+			}
+
+			strLen = 0
+			for i := 0; i < len(v); i++ {
+				if v[i] == '\\' {
+					i++
+					if i + 2 < len(v) && isDigit(v[i]) && isDigit(v[i + 1]) && isDigit(v[i + 2]) {
+						i += 2
+					}
+				}
+				strLen++
+			}
+			strLen += len(k) + 1
+			if strLen > 255 {
+				return nil, NewDirectorError(ErrDirWrongTxtString, "TXT string exceeded 255 bytes")
+			}
+			fullLen += strLen + 1
+
+			if(fullLen > 1300) {
+				return nil, NewDirectorError(ErrDirWrongTxtString, "String section of a TXT resource record exceeded 1300 bytes")
+			}
+			if k == "txtvers" {
+				str[0] = k + "=" + v
+			} else {
+				str = append(str, k + "=" + v)
+			}
+		}	
+		if str[0] == "" {
+			str = str[1:]
+		}
+	}
+
+	txt := new(dns.TXT)
+	txt.Hdr = dns.RR_Header{srvName, dns.TypeTXT, dns.ClassINET, 0, 0}
+	txt.Txt = str
+	return txt, nil
+}
+
 func isNotAllowedCharacter(c rune) bool {
 	return !unicode.IsLetter(c) && !unicode.IsNumber(c) && c != '_'
 }
@@ -106,10 +193,31 @@ func validateSrvNameWithoutType(srvName string) error {
 		return NewDirectorError(ErrDirWrongSrvName, "Service name coincides with a service type")
 	}
 	if pos := strings.IndexFunc(srvName, isNotAllowedCharacter); pos != -1 {
-		return NewDirectorError(ErrDirWrongSrvType, "Starting part of a service name contains not allowed character '%s'", srvName[pos:pos + 1])
+		return NewDirectorError(ErrDirWrongSrvName, "Starting part of a service name contains not allowed character '%s'", srvName[pos:pos + 1])
 	}
 	if srvName[0] == '_' {
-		return NewDirectorError(ErrDirWrongSrvType, "Service name mut not start with '_'")
+		return NewDirectorError(ErrDirWrongSrvName, "Service name mut not start with '_'")
+	}
+	return nil
+}
+
+func validateSrvName(srvName string) error {
+	if srvName == "" {
+		return NewDirectorError(ErrDirWrongSrvName, "Service name is empty")
+	}
+
+	if srvName[0] == '_' {
+		return NewDirectorError(ErrDirWrongSrvName, "Service name must not start with '_'")
+	}
+
+	parts := strings.Split(srvName, ".")
+	for i, p := range parts {
+		if pos := strings.IndexFunc(p, isNotAllowedCharacter); pos != -1 {
+			return NewDirectorError(ErrDirWrongSrvName, "Service name contains not allowed character '%s'", p[pos:pos + 1])
+		}
+		if i > 0 && p[0] != '_' {
+			return NewDirectorError(ErrDirWrongSrvName, "Middle service name's parts must start with '_'")
+		}
 	}
 	return nil
 }
